074cfda814fcd144531563113a9ea3af0bef84ee
Ivan Aleksandrov <Ivan.A-1996@mail.ru>
2018-11-05 17:57:33 +0200

    fix select language on ios

--- ios/RNTesseractOcr.m
+++ ios/RNTesseractOcr.m
@@ -17,51 +17,50 @@
     return YES;
 }
 
-- (NSDictionary *)constantsToExport
-{
-    return @{
-             @"LANG_AFRIKAANS": @"afr",
-             @"LANG_AMHARIC": @"amh",
-             @"LANG_ARABIC": @"ara",
-             @"LANG_ASSAMESE": @"asm",
-             @"LANG_AZERBAIJANI": @"aze",
-             @"LANG_BELARUSIAN": @"bel",
-             @"LANG_BOSNIAN": @"bos",
-             @"LANG_BULGARIAN": @"bul",
-             @"LANG_CHINESE_SIMPLIFIED": @"chi_sim",
-             @"LANG_CHINESE_TRADITIONAL": @"chi_tra",
-             @"LANG_CROATIAN": @"hrv",
-             @"LANG_CUSTOM": @"custom",
-             @"LANG_DANISH": @"dan",
-             @"LANG_ENGLISH": @"eng",
-             @"LANG_ESTONIAN": @"est",
-             @"LANG_FRENCH": @"fra",
-             @"LANG_GALICIAN": @"glg",
-             @"LANG_GERMAN": @"deu",
-             @"LANG_HEBREW": @"heb",
-             @"LANG_HUNGARIAN": @"hun",
-             @"LANG_ICELANDIC": @"isl",
-             @"LANG_INDONESIAN": @"ind",
-             @"LANG_IRISH": @"gle",
-             @"LANG_ITALIAN": @"ita",
-             @"LANG_JAPANESE": @"jpn",
-             @"LANG_KOREAN": @"kor",
-             @"LANG_LATIN": @"lat",
-             @"LANG_LITHUANIAN": @"lit",
-             @"LANG_NEPALI": @"nep",
-             @"LANG_NORWEGIAN": @"nor",
-             @"LANG_PERSIAN": @"fas",
-             @"LANG_POLISH": @"pol",
-             @"LANG_PORTUGUESE": @"por",
-             @"LANG_RUSSIAN": @"rus",
-             @"LANG_SERBIAN": @"srp",
-             @"LANG_SLOVAK": @"slk",
-             @"LANG_SPANISH": @"spa",
-             @"LANG_SWEDISH": @"swe",
-             @"LANG_TURKISH": @"tur",
-             @"LANG_UKRAINIAN": @"ukr",
-             @"LANG_VIETNAMESE": @"vie"
-             };
+- (NSString*) getConstants:(NSString*)language  {
+    NSDictionary *wordDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
+                                        @"afr", @"LANG_AFRIKAANS",
+                                        @"amh", @"LANG_AMHARIC",
+                                        @"ara", @"LANG_ARABIC",
+                                        @"asm", @"LANG_ASSAMESE",
+                                        @"aze", @"LANG_AZERBAIJANI",
+                                        @"bel", @"LANG_BELARUSIAN",
+                                        @"bos", @"LANG_BOSNIAN",
+                                        @"bul", @"LANG_BULGARIAN",
+                                        @"chi_sim", @"LANG_CHINESE_SIMPLIFIED",
+                                        @"chi_tra", @"LANG_CHINESE_TRADITIONAL",
+                                        @"hrv", @"LANG_CROATIAN",
+                                        @"custom", @"LANG_CUSTOM",
+                                        @"dan", @"LANG_DANISH",
+                                        @"eng", @"LANG_ENGLISH",
+                                        @"est", @"LANG_ESTONIAN",
+                                        @"fra", @"LANG_FRENCH",
+                                        @"glg", @"LANG_GALICIAN",
+                                        @"deu", @"LANG_GERMAN",
+                                        @"heb", @"LANG_HEBREW",
+                                        @"hun", @"LANG_HUNGARIAN",
+                                        @"isl", @"LANG_ICELANDIC",
+                                        @"ind", @"LANG_INDONESIAN",
+                                        @"gle", @"LANG_IRISH",
+                                        @"ita", @"LANG_ITALIAN",
+                                        @"jpn", @"LANG_JAPANESE",
+                                        @"kor", @"LANG_KOREAN",
+                                        @"lat", @"LANG_LATIN",
+                                        @"lit", @"LANG_LITHUANIAN",
+                                        @"nep", @"LANG_NEPALI",
+                                        @"nor", @"LANG_NORWEGIAN",
+                                        @"fas", @"LANG_PERSIAN",
+                                        @"pol", @"LANG_POLISH",
+                                        @"por", @"LANG_PORTUGUESE",
+                                        @"rus", @"LANG_RUSSIAN",
+                                        @"srp", @"LANG_SERBIAN",
+                                        @"slk", @"LANG_SLOVAK",
+                                        @"spa", @"LANG_SPANISH",
+                                        @"swe", @"LANG_SWEDISH",
+                                        @"tur", @"LANG_TURKISH",
+                                        @"ukr", @"LANG_UKRAINIAN",
+                                        @"vie", @"LANG_VIETNAMESE", nil];
+    return [wordDictionary valueForKey: language];
 }
 
 RCT_EXPORT_MODULE()
@@ -72,56 +71,56 @@ RCT_EXPORT_METHOD(recognize:(nonnull NSString*)path
                   rejecter:(RCTPromiseRejectBlock)reject)
 {
     RCTLogInfo(@"starting Ocr");
-    
-    _tesseract = [[G8Tesseract alloc] initWithLanguage:language];
+
+    _tesseract = [[G8Tesseract alloc] initWithLanguage: [self getConstants:language]];
 //    _tesseract.image = [[UIImage imageWithData:[NSData dataWithContentsOfFile:path]] g8_blackAndWhite];
-    
+
     UIImage *originImg = [UIImage imageNamed:path];
     _tesseract.image = [self processImage:originImg];
-    
+
     _tesseract.engineMode = G8OCREngineModeTesseractOnly;
     _tesseract.pageSegmentationMode = G8PageSegmentationModeAuto;
     //_tesseract.delegate = self;
-    
+
     if(options != NULL) {
         NSString *whitelist = [options valueForKey:@"whitelist"];
         if(![whitelist isEqual: [NSNull null]] && [whitelist length] > 0){
             _tesseract.charWhitelist = whitelist;
         }
-        
+
         NSString *blacklist = [options valueForKey:@"blacklist"];
         if(![blacklist isEqual: [NSNull null]] && [blacklist length] > 0){
             _tesseract.charBlacklist = blacklist;
         }
-        
+
 //        NSString *characterChoices = [options valueForKey:@"characterChoices"];
 //        if([blacklist length] > 0){
 //            _tesseract.characterChoices = blacklist;
 //        }
     }
-    
+
     BOOL success = _tesseract.recognize;
     NSString *recognizedText = _tesseract.recognizedText;
-    
-    NSArray *characterBoxes = [_tesseract recognizedBlocksByIteratorLevel:G8PageIteratorLevelSymbol];
-    NSMutableArray *boxes = [[NSMutableArray alloc] initWithCapacity:characterBoxes.count];
-    
-    for (G8RecognizedBlock *block in characterBoxes) {
-        [boxes addObject:@{
-                           @"text" : block.text,
-                           @"boundingBox" : @{
-                                   @"x": [NSNumber numberWithFloat:block.boundingBox.origin.x],
-                                   @"y": [NSNumber numberWithFloat:block.boundingBox.origin.y],
-                                   @"width": [NSNumber numberWithFloat:block.boundingBox.size.width],
-                                   @"height": [NSNumber numberWithFloat:block.boundingBox.size.height]
-                                   },
-                           @"confidence" : [NSNumber numberWithFloat:block.confidence],
-                           @"level" : [NSNumber numberWithInt:block.level]
-                           }];
-    }
-    
+
+//    NSArray *characterBoxes = [_tesseract recognizedBlocksByIteratorLevel:G8PageIteratorLevelSymbol];
+//    NSMutableArray *boxes = [[NSMutableArray alloc] initWithCapacity:characterBoxes.count];
+//
+//    for (G8RecognizedBlock *block in characterBoxes) {
+//        [boxes addObject:@{
+//                           @"text" : block.text,
+//                           @"boundingBox" : @{
+//                                   @"x": [NSNumber numberWithFloat:block.boundingBox.origin.x],
+//                                   @"y": [NSNumber numberWithFloat:block.boundingBox.origin.y],
+//                                   @"width": [NSNumber numberWithFloat:block.boundingBox.size.width],
+//                                   @"height": [NSNumber numberWithFloat:block.boundingBox.size.height]
+//                                   },
+//                           @"confidence" : [NSNumber numberWithFloat:block.confidence],
+//                           @"level" : [NSNumber numberWithInt:block.level]
+//                           }];
+//    }
+
     resolve([NSString stringWithFormat:@"%@", recognizedText]);
-    
+
     // reject(@"no_events", @"There were no events", error);
 }
 
@@ -149,14 +148,14 @@ RCT_EXPORT_METHOD(recognize:(nonnull NSString*)path
 //    CGColorSpaceRelease(colorSpace);
 //    CGContextRelease(context);
 //    CFRelease(imageRef);
-    
+
     // Initialize our adaptive threshold filter
     GPUImageAdaptiveThresholdFilter *stillImageFilter = [[GPUImageAdaptiveThresholdFilter alloc] init];
     stillImageFilter.blurRadiusInPixels = 12.0; // adjust this to tweak the blur radius of the filter, defaults to 4.0
 
     // Retrieve the filtered image from the filter
     UIImage *newImage = [stillImageFilter imageByFilteringImage:image];
-    
+
     return newImage;
 }
 
